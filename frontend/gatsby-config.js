75a1ad29cb233cefec51b01b5e76d3f610a3d0ff
Altrum <robertchung97@gmail.com>
2020-11-16 23:02:03 -0800

    Gtag

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -91,6 +91,28 @@ module.exports = {
         start_url: `/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-DKP94LCN31", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: false,
+        },
+      },
+    },
   ],
 
     // this (optional) plugin enables Progressive Web App + Offline functionality
